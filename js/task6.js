c62041f24e3cb2a34bd35ebe39c03836b64b4fc4
nyxeet <nyxeet@ukr.net>
2020-11-22 20:30:10 +0200

    fds

---
+++ js/task6.js
@@ -0,0 +1,24 @@
+const inputRef = document.querySelector('input[id="validation-input"]');
+console.log(inputRef);
+
+inputRef.addEventListener('input', clearClasses);
+inputRef.addEventListener('blur', isValidate);
+
+function clearClasses(event) {
+    if (!event.target.value) {
+        inputRef.classList.remove('valid', 'invalid');
+    }
+}
+function isValidate(event) {
+    const inputAttribute = inputRef.getAttribute('data-length');
+
+    if (event.target.value.length == inputAttribute) {
+        inputRef.classList.add('valid')
+        inputRef.classList.remove('invalid');
+    } else if (event.target.value.length == 0) {
+        inputRef.classList.remove('valid', 'invalid');
+    } else {
+        inputRef.classList.add('invalid')
+        inputRef.classList.remove('valid');
+    }
+}
